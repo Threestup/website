479eb5f8dd5d8cd518424f83f565cc6997ee08a7
Slavomir Vojacek <slavomirvojacek@gmail.com>
2018-01-30 22:08:22 +0700

    chore(Form): Init work on modal

--- src/common/contact-form.php
+++ src/common/contact-form.php
@@ -1,5 +1,6 @@
-<section class="padded contact-form">
-  <div class="container">
+<div id="modal-backdrop">
+  <div class="modal container">
+    <strong class="title">Let's work together!</strong>
     <form action="http://0.0.0.0:1789/aporosa/contact" method="POST">
       Your Name:<br>
       <input type="text" name="name" value="Your name"><br>
@@ -9,7 +10,7 @@
       <input type="text" name="organisation" value="Your organisation (optional)"><br>
       Message:<br>
       <input type="text" name="message" value="Type your message here (optional)"><br>
-      <input type="submit" value="Submit">
+      <input class="button" type="submit" value="Submit">
     </form>
   </div>
-</section>
+</div>
