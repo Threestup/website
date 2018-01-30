c991c57fd7a594d36273419874c5b08c1111cdc8
Anas AIT ALI <aitali.anas@gmail.com>
2018-01-30 22:37:02 +0000

    prepare layout for arrows

--- src/common/how-we-work.php
+++ src/common/how-we-work.php
@@ -1,35 +1,36 @@
 <section class="padded how-we-work" id="how-we-work">
     <div class="container">
         <h2>How we work</h2>
-        <div class="row items-list">
-            <div class="col-lg-4 items-list-item">
+        <div class="row items-list mb-5">
+            <div class="col-lg-5 items-list-item">
                 <strong class="title">1. Let's have a chat</strong>
                 <p>
-                    Our hourly rate is fixed and the total cost will be based on
-                    what we estimated and agreed on together in the planning stage.
+                    Our hourly rate is fixed and the total cost will be based on what we estimated and agreed on together in the planning stage.
                 </p>
                 <p>
                     If your requirements change, we provide an up-to-date estimation.
                 </p>
             </div>
-            <div class="col-lg-4 items-list-item">
+        </div>
+        <div class="row items-list mb-5">
+            <div class="col-lg-5 offset-md-7 items-list-item">
                 <strong class="title">2. Time to estimate</strong>
                 <p>
-                    Within two business days our team will work on your project’s
-                    requirements and provide an estimate of hours to complete it.
+                    Within two business days our team will work on your project’s requirements and provide an estimate of hours to complete it.
                 </p>
                 <p>
                     The best part? All our quotations are free of charge.
                 </p>
             </div>
-            <div class="col-lg-4 items-list-item">
+        </div>
+        <div class="row items-list">
+            <div class="col-lg-5 items-list-item">
                 <strong class="title">3. We're on it!</strong>
                 <p>
                     At this stage, we plan the work and go heads-down coding!
                 </p>
                 <p>
-                    We will communicate our progress as well as possible
-                    project changes on a daily basis.
+                    We will communicate our progress as well as possible project changes on a daily basis.
                 </p>
             </div>
         </div>
