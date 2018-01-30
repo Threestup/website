36c4134420434e15ec9855c4d8ff8336ad27a89b
Anas AIT ALI <aitali.anas@gmail.com>
2018-01-30 12:41:37 +0000

    GA

--- src/common/head.php
+++ src/common/head.php
@@ -10,4 +10,13 @@
 
     <link href="https://fonts.googleapis.com/css?family=Volkhov:700|Roboto+Mono:400,700" rel="stylesheet">
     <link href="/css/app.css" rel="stylesheet">
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110892605-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-110892605-1');
+    </script>
+
 </head>
