c9cf79d012d4aa9bf07b82bc372cbd0d3dea6af4
Slavomir Vojacek <slavomirvojacek@gmail.com>
2018-01-31 17:59:19 +0700

    style(JS): Minor JS style improvements

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,47 @@
 (function ($) {
   const doc = $(document);
   const win = $(window);
-  
+
   let header = null;
   let expandMenuDrawer = null;
   let menuDrawer = null;
   let modal = null;
   let showContactForm = null;
-  
+  let hideContactForm = null;
+
   doc.ready((_event) => {
     header = $("#white-bg-on-scroll");
     expandMenuDrawer = $("#expand-menu-drawer");
     menuDrawer = $("#menu-drawer");
     modal = $("#modal-backdrop");
     showContactForm = $(".show-contact-form");
+    hideContactForm = $("#modal-backdrop .close-btn");
 
     init();
   });
-  
+
   function init() {
     win.scroll(onScroll);
     onScroll();
-  
+
     expandMenuDrawer.click((_event) => {
       expandMenuDrawer.toggleClass("active");
       menuDrawer.toggleClass("active");
     });
-  
-    $('#menu-drawer > li a:not(.nav-button)').click(function(e) {
-      var $this = $(this);
+
+    $('#menu-drawer > li a:not(.nav-button)').click((_event) => {
+      const $this = $(this);
       $this.parent().siblings().removeClass('active').end().addClass('active');
     });
 
-    showContactForm.click((_e) => {
-      modal.addClass("active").animate({opacity: 1});
+    showContactForm.click((_event) => {
+      modal.addClass("active").animate({ opacity: 1 });
     });
 
-    $("#modal-backdrop .close-btn").click((_e) => {
-      modal.animate({opacity: 0}, function() { $(this).removeClass("active") });
+    $("#modal-backdrop .close-btn").click((_event) => {
+      modal.animate({ opacity: 0 }, () => {
+        modal.removeClass("active");
+      });
     });
 
     new SmoothScroll("a[href*=\"#\"]", {
@@ -45,23 +49,23 @@
       offset: 80,
       easing: "easeInOutCubic"
     });
+
     // TODO: Replace scrollspy or smoothscroll as they conflict
     // menuDrawer.scrollspy({ offset: -100 });
   }
-  
+
   function toggleHeaderSolidClass(amount) {
     const hasSolidClass = header.hasClass("solid-bg");
-  
+
     if (amount === 0) {
-      header.removeClass("solid-bg")
+      header.removeClass("solid-bg");
     } else if (!hasSolidClass) {
       header.addClass("solid-bg");
     }
   }
-  
+
   function onScroll() {
     const amount = win.scrollTop();
     toggleHeaderSolidClass(amount);
   }
-  
 })(jQuery);
