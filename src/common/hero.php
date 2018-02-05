896b0074a1d0eddc8168203a371a08b3e70bee41
Slavomir Vojacek <slavomirvojacek@gmail.com>
2018-02-05 19:36:21 +0700

    chore(Copy): Update copy to match manifesto

--- src/common/hero.php
+++ src/common/hero.php
@@ -1,6 +1,6 @@
 <section class="hero" id="home">
     <div class="container hero-content">
-      <h1>We 🛠 plug-and-play MVPs,<br> Proofs of Concept,<br> and provide Mentoring</h1>
+      <h1>We 🛠 useful Software,<br> future-proof MVPs,<br> and provide Mentoring</h1>
       <a class="button red show-contact-form">Get Started</a>
     </div>
 </section>
