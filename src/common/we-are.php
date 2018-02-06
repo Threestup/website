1682fbfa7aca73b10c406a1dc0e0747d90557743
Slavomir Vojacek <slavomirvojacek@gmail.com>
2018-02-06 23:27:21 +0700

    chore(Copy): About us copy update

--- src/common/we-are.php
+++ src/common/we-are.php
@@ -1,15 +1,15 @@
 <section class="padded we-are" id="about-us">
     <div class="container">
         <h2>About us</h2>
-        <p>We are Anas, Jeremy, and Slavo &ndash; a group of friends who love what they do.</p>
+        <p>We are Anas, Jeremy, and Slavo &ndash; a group of friends and colleagues who love what they do.</p>
         <p>Before Threestup, we worked together on numerous projects, where we
         found our shared passion for technology,
         and for doing things the way we believe is right.
-        In other words, we value <strong>quality, impact, and value.</strong></p>
-        <p>After a short break and countless hours spent
-        in cafes around London, we decided to start helping companies
-        solve software and product challenges together.</p>
-        <p>If you are one of them, <span class="link funky show-contact-form">drop us a line</span>, we'd love to chat!</p>
+        In other words, we value <strong>quality, impact, and value</strong> highly.</p>
+        <p>After a while, solving software and product challenges as individuals
+        simply wasn't enough, and we decided to combine
+        our skillsets and start helping businesses together.</p>
+        <p>Interested in working with us? <span class="link funky show-contact-form">Drop us a line</span>, we'd love to chat!</p>
         <p><small>PS: We also like to travel, so moving to another country for <i>any</i> duration of your project is not a problem :)</small></p>
     </div>
 </section>
