e326eebe26c03a1d04c013290517ddc7beb9a2ec
Slavomir Vojacek <slavomirvojacek@gmail.com>
2018-02-06 23:08:06 +0700

    chore(Copy): Be more professional

--- src/common/we-are.php
+++ src/common/we-are.php
@@ -3,10 +3,12 @@
         <h2>About us</h2>
         <p>We are Anas, Jeremy, and Slavo &ndash; a group of friends who love what they do.</p>
         <p>Before Threestup, we worked together on numerous projects, where we
-        noticed our shared passion for technology, and for doing things well.</p>
-        <p>After a short break and countless hours spent in cafes around London,
-        we decided to give it a shot and started contacting people and companies
-        who might need help solving software and product challenges.</p>
+        found our shared passion for technology,
+        and for doing things the way we believe is right.
+        In other words, we value <strong>quality, impact, and value.</strong></p>
+        <p>After a short break and countless hours spent
+        in cafes around London, we decided to start helping companies
+        solve software and product challenges together.</p>
         <p>If you are one of them, <span class="link funky show-contact-form">drop us a line</span>, we'd love to chat!</p>
         <p><small>PS: We also like to travel, so moving to another country for <i>any</i> duration of your project is not a problem :)</small></p>
     </div>
