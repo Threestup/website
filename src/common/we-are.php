7cabe61eb3939e5d26e2b65fad754ab118136a75
Shad Deen <>
2022-06-30 17:24:27 +0100

    Update copy

--- src/common/we-are.php
+++ src/common/we-are.php
@@ -1,11 +1,13 @@
 <section class="padded we-are" id="about-us">
     <div class="container">
         <h2>About us</h2>
-        <p>We are a purposefully-small team of senior consultant software engineers with a shared passion for technology and projects.</p>
-        <p>With our expertise and diverse skills, we help companies bring their ideas to life with a hassle-free approach. </p>
-      
+        
+        <p>We are a team of passionate senior consultant software engineers with a shared love for technology and problem-solving.</p>
+        <p>We help companies bring their ideas to life with a hassle-free approach and diverse range of skills.</p>
         <p>We pride ourselves upon our strong commitment to clean, efficient and high quality work.</p>
         <p>We craft easy-to-use, scalable and performant software for our clients.</p>
+
+        <p>Our clients come from various industries such as but not limited to, Fintech, Healthcare, Entertainment, Lifestyle, Logistics</p>
       
         <p>Interested in working with us? <span class="link funky show-contact-form">Drop us a line</span>, we'd love to chat!</p>
     </div>
